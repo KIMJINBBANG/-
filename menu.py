b5bb01ae78fc0eaa77c2f2c1449480926ac59c58
toby0204 <ekffo0204@kau.kr>
2022-04-13 23:08:09 +0900

    v1

---
+++ menu.py
@@ -0,0 +1,23 @@
+
+print("==========Bank Menu==================")
+print(
+print("=========================================")
+answer=input("입 력")
+
+if (answer==1):
+    make_account()
+
+else if (answer==2):
+    add_money()
+
+else if (answer==3):
+    out_money()
+
+else if (answer==4)
+    search()
+
+else if (answer==5)
+    free_option()
+
+else (answer==6)
+    exit()
