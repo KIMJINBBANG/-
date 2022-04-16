cb01bb3a19b90e5fcf9d4064fb4c7abddf72f88f
KIMJINBBANG <0925taekwonjy@gmail.com>
2022-04-16 15:38:52 +0900

    v2.1

--- bank.py
+++ bank.py
@@ -1,3 +1,9 @@
+# 계좌번호 중복방지  = 완성
+# 출금금액이 잔액보다 큰경우 방지 = 완성
+# 금액이 문자인경우/ 이름이 숫자인경우
+
+
+
 from information import account
 total = {}
 
@@ -18,8 +24,23 @@ while True:
     if question == "1" :
         print("======계좌개설======")
         accountnumber = input("계좌번호를 입력해주세요 : ")
+        if accountnumber in total :
+            print("===이미 존재하는 계좌번호입니다.===")
+            continue
+        else :
+            pass
         name = input("이름을 입력해주세요 : ")
+        if name.isdigit() :
+            print("===잘못된 입력입니다.===")
+            continue
+        else :
+            pass
         money = input("입금할 금액을 입력해주세요 :")
+        if money.isalpha() :
+            print("===잘못된 입력입니다.===")
+            continue
+        else :
+            pass
         newaccount = account.Main_account(accountnumber, name, money)
         total[accountnumber] = newaccount
         print("##계좌개설을 완료하였습니다##")
@@ -30,8 +51,13 @@ while True:
         identify = input("입금하실 계좌번호를 입력해주세요 : ")
         if identify in total :
             print("계좌이름 : ", total[identify].name)
-            print("계좌잔고 : ", total[identify].money)            
+            print("계좌잔고 : ", total[identify].money, "원")            
             depositmoney = input("입금하실 금액을 입력해주세요 : ")
+            if depositmoney.isalpha() :
+                print("===잘못된 입력입니다.===")
+                continue
+            else :
+                pass
             total[identify].deposit(int(depositmoney))
             print()
             print("##계좌잔고 : ", total[identify].money, "원##")
@@ -46,8 +72,18 @@ while True:
         identify = input("출금하실 계좌번호를 입력해주세요 : ")
         if identify in total :
             print("계좌이름 : ", total[identify].name)
-            print("계좌잔고 : ", total[identify].money)            
+            print("계좌잔고 : ", total[identify].money , "원")            
             withdrawmoney = input("출금하실 금액을 입력해주세요 : ")
+            if withdrawmoney.isalpha() :
+                print("===잘못된 입력입니다.===")
+                continue
+            else :
+                pass
+            if int(withdrawmoney) > int(total[identify].money) :
+                print("잔액 부족")
+                continue
+            else :
+                pass
             total[identify].withdraw(int(withdrawmoney))
             print()
             print("##계좌잔고 : ", total[identify].money, "원##")
@@ -60,7 +96,8 @@ while True:
     elif question == "4" :
         print("======전체조회======")
         for i in total:
-            print("계좌번호 : ", total[i].account, "/ 이름 :", total[i].name, "/ 잔액 : ", total[i].money)
+            print("계좌번호 : ", total[i].account, "/ 이름 :", total[i].name, "/ 잔액 : ", total[i].money, "원")
+        print("====================")
 
     
     elif question == "5" :
